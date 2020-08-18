39249741f06842679d677cac83e175a4dfc46a1a
favourOkeibunor <favourokeibunor@gmail.com>
2020-08-18 11:18:56 +0100

    send message and close connection

--- send.php
+++ send.php
@@ -3,6 +3,19 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+
 // create a connection to the server
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
+
+// creating a channel
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+// close the channel and close the connection 
+$channel->close();
+$connection->close();
